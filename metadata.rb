84b57c73dd8c0e1c450d32a70379a38af9fe5248
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-12-02 08:57:27 -0800

    added name to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
 license          "Apache 2.0"
 description      "Distributes ohai plugins for vagrant chef nodes"
+name		 "vagrant-ohai"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
